8e1ffbaad427156f0a396fed377359e684adfe7b
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-09 19:41:55 +0000

    Add type

---
+++ use-cases/src/lib/user.ts
@@ -0,0 +1,17 @@
+import { identity } from 'fp-ts/function';
+import * as t from 'io-ts';
+
+export type User = {
+  name: string;
+  age: number;
+};
+
+export type DecodedUser = t.TypeOf<typeof User>;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _ = (u: User): DecodedUser => identity(u);
+
+export const User = t.type({
+  name: t.string,
+  age: t.number,
+});
