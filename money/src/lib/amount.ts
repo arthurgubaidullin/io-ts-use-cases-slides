65a762f7b02a00c760077f98d4de370cc2479f95
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-10 07:22:32 +0000

    Fix type

--- money/src/lib/amount.ts
+++ money/src/lib/amount.ts
@@ -5,7 +5,7 @@ export type Amount = t.TypeOf<typeof Amount>;
 export const Amount = t.Int;
 
 export const create = (data: { amount: number }): Amount =>
-  Math.min(0, data.amount) as Amount;
+  Math.max(0, data.amount) as Amount;
 
 export const add = (amount1: Amount, amount2: Amount): Amount =>
   (amount1 + amount2) as Amount;
