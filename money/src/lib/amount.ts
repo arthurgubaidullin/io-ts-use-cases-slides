446e4b9ae21349851b63d6905225c8c908feb620
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-10 07:18:51 +0000

    Add type

---
+++ money/src/lib/amount.ts
@@ -0,0 +1,11 @@
+import * as t from 'io-ts';
+
+export type Amount = t.TypeOf<typeof Amount>;
+
+export const Amount = t.Int;
+
+export const create = (data: { amount: number }): Amount =>
+  Math.min(0, data.amount) as Amount;
+
+export const add = (amount1: Amount, amount2: Amount): Amount =>
+  (amount1 + amount2) as Amount;
