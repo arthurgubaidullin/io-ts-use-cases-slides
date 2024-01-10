3c2cd3faaa49e60a20654aca9884cc27415efd40
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-10 07:32:15 +0000

    Add codec & type

--- money-codec/src/lib/money-codec.spec.ts
+++ money-codec/src/lib/money-codec.spec.ts
@@ -1,7 +1,27 @@
-import { moneyCodec } from './money-codec';
+import { pipe } from 'fp-ts/function';
+import { Money } from '@w/money';
+import { MoneyCodec } from './money-codec';
+import * as E from 'fp-ts/Either';
+import * as PathReporter from 'io-ts/PathReporter';
+import * as t from 'io-ts';
 
-describe('moneyCodec', () => {
-  it('should work', () => {
-    expect(moneyCodec()).toEqual('money-codec');
-  });
+const money = new Money({ amount: 10 as t.Int, currency: 'GEL' });
+
+describe('Money', () => {
+  it('should encode & decode money class', () =>
+    pipe(
+      money,
+      MoneyCodec.encode,
+      MoneyCodec.decode,
+      E.mapLeft(PathReporter.failure),
+      E.fold(
+        (es) => {
+          throw new Error(es.join('\n'));
+        },
+        (_money) => {
+          expect(_money).toBeInstanceOf(Money);
+          expect(_money.equals(money)).toBeTruthy();
+        }
+      )
+    ));
 });
--- money-codec/src/lib/money-codec.ts
+++ money-codec/src/lib/money-codec.ts
@@ -1,3 +1,16 @@
-export function moneyCodec(): string {
-  return 'money-codec';
-}
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as t from 'io-ts';
+import { Money } from '@w/money';
+import { MoneyStruct } from '@w/money-struct';
+
+export const MoneyCodec = new t.Type<Money, MoneyStruct, unknown>(
+  'Money',
+  (a): a is Money => a instanceof Money,
+  (u, c) =>
+    pipe(
+      MoneyStruct.validate(u, c),
+      E.map((struct) => new Money(struct))
+    ),
+  ({ amount, currency }) => ({ amount, currency })
+);
