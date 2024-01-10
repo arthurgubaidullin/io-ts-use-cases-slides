5bead0a5e028b60f2bce160bf5f2fdcbcb27c759
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-10 07:31:28 +0000

    Add codec & type

--- money-struct/src/lib/money-struct.spec.ts
+++
@@ -1,7 +0,0 @@
-import { moneyStruct } from './money-struct';
-
-describe('moneyStruct', () => {
-  it('should work', () => {
-    expect(moneyStruct()).toEqual('money-struct');
-  });
-});
--- money-struct/src/lib/money-struct.ts
+++ money-struct/src/lib/money-struct.ts
@@ -1,3 +1,13 @@
-export function moneyStruct(): string {
-  return 'money-struct';
-}
+import { Currency } from '@w/currency';
+import * as t from 'io-ts';
+
+export type MoneyStruct = t.TypeOf<typeof MoneyStruct>;
+
+export const MoneyStruct = t.readonly(
+  t.exact(
+    t.type({
+      amount: t.Int,
+      currency: Currency,
+    })
+  )
+);
