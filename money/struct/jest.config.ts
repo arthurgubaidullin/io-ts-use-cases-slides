ed9d33ec5fb3e9abffae79f408c0e13437a54a6a
Artur Gubaidullin <lapteuh@gmail.com>
2024-01-10 07:35:31 +0000

    Move project

--- money/struct/jest.config.ts
+++ money/struct/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'money-struct',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/money-struct',
+  coverageDirectory: '../../coverage/money/struct',
 };
